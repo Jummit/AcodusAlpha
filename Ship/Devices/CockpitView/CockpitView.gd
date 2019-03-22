5828286ac2f541cfd837df11b9ca01a139c759b6
Jummit <jummit@web.de>
2019-03-22 21:31:18 +0100

    added a cockpit device

---
+++ Ship/Devices/CockpitView/CockpitView.gd
@@ -0,0 +1,5 @@
+extends "res://Ship/Devices/Device.gd"
+
+"""
+Shows the space outside in 3D projected by a viewport.
+"""
---
+++ Ship/Devices/Device.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+"""
+A device that can be placed in a room
+
+Used to create all kinds of devices, for examples farming plots, screens, furniture and more.
+"""
