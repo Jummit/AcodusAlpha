bb7cacbd30a0ee0d31c5273a7ed5b20cb0266985
Jummit <jummit@web.de>
2019-03-23 11:22:26 +0100

    completed spaceship controlls

--- ShipInterior/Devices/CockpitView/CockpitView.gd
+++ ShipInterior/Devices/CockpitView/CockpitView.gd
@@ -10,13 +10,6 @@ onready var ship : = $ViewportContainer/Viewport/Ship
 
 var selected : bool setget set_selected, get_selected
 
-class MovementInput:
-	var movement : Vector3
-	var rotation : Vector3
-	func _init(movement : Vector3, rotation : Vector3):
-		self.movement = movement
-		self.rotation = rotation
-
 func set_selected(v : bool):
 	selected = v
 	ship_camera.set_process(not selected)
@@ -25,40 +18,38 @@ func get_selected() -> bool:
 	return selected
 
 
-func _get_movement_input() -> MovementInput:
-	var input : MovementInput = MovementInput.new(Vector3(0, 0, 0), Vector3(0, 0, 0))
+func _get_movement_input() -> Vector3:
+	var input = Vector3(0, 0, 0)
 	
-	if Input.is_key_pressed(KEY_A):
-		input.rotation.x = 1
-	elif Input.is_key_pressed(KEY_D):
-		input.rotation.x = -1
+	if Input.is_action_pressed("left"):
+		input.x = -1
+	elif Input.is_action_pressed("right"):
+		input.x = 1
 	
-	if Input.is_key_pressed(KEY_Q):
-		input.rotation.y = 1
-	elif Input.is_key_pressed(KEY_E):
-		input.rotation.y = -1
+	if Input.is_action_pressed("forward"):
+		input.z = -1
+	elif Input.is_action_pressed("backward"):
+		input.z = 1
 	
-	if Input.is_key_pressed(KEY_W):
-		input.movement.z = -1
-	elif Input.is_key_pressed(KEY_S):
-		input.movement.z = 1
+	if Input.is_action_pressed("up"):
+		input.y = -1
+	elif Input.is_action_pressed("down"):
+		input.y = 1
 	
 	return input
 
 
-func process_movement_input(input : MovementInput):
-	# setup movement
-	input.movement /= 10
-	input.movement = input.movement.rotated(Vector3(1, 0, 0), ship.rotation.x)
-	input.movement = input.movement.rotated(Vector3(0, 1, 0), ship.rotation.y)
+func process_movement_input(input : Vector3):
+	input = Transform(self.ship.transform.basis, Vector3(0, 0, 0)).xform(input)
+	self.ship.translation += input / 10
+
+func process_rotation_input(input : Vector3):
+	input /= 40
+	input = -input
 	
-	input.rotation /= 50
-	
-	# move and rotate ship
-	ship.rotation.y += input.rotation.x
-	ship.rotation.x += input.rotation.y
-	
-	ship.translation += input.movement
+	self.ship.rotate_object_local(Vector3(1, 0, 0), input.z)
+	self.ship.rotate_object_local(Vector3(0, 1, 0), input.x)
+	self.ship.rotate_object_local(Vector3(0, 0, 1), input.y)
 
 
 func _process(delta : float) -> void:
@@ -66,7 +57,11 @@ func _process(delta : float) -> void:
 		set_selected(false)
 	
 	if selected:
-		process_movement_input(_get_movement_input())
+		var input : Vector3 = _get_movement_input()
+		if Input.is_action_pressed("rotation_switch"):
+			process_rotation_input(input)
+		else:
+			process_movement_input(input)
 
 
 func _on_input_event(viewport, event, shape_idx):
